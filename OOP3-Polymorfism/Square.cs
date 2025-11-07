ee3fb9a125c433c6eb9f16330e72c06f0c406997
tonilingardsson <toni.lingardsson@gmail.com>
2025-11-07 12:43:30 +0100

    Add Square class implementing Geometry with Area method

---
+++ OOP3-Polymorfism/Square.cs
@@ -0,0 +1,21 @@
+namespace Polymorphism
+{
+  public class Square : Geometry
+  {
+    // Declaring Side property and its type
+    public double Side {get; set;}
+
+    // Constructor with Side = 8, we need to get an area = 64, 8x8
+    public Square()
+    {
+        Side = 8; 
+    }
+
+    // Declaring Square's method for Area
+    public override double Area()
+    {
+        // Square's area is side^2
+        return Side*Side;
+    } 
+  }  
+}
