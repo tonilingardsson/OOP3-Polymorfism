232818c7e97d8005b4871815b365f5f16ea7aed7
tonilingardsson <toni.lingardsson@gmail.com>
2025-11-07 19:35:32 +0100

    Fix namespace spelling typo and add using directives for Triangle class

--- OOP3-Polymorfism/Triangle.cs
+++ OOP3-Polymorfism/Triangle.cs
@@ -1,4 +1,10 @@
-namespace Polymophism
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
 {
     public class Triangle : Geometry
     {
