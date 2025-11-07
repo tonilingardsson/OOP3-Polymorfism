4e01e68fbf9dd934fadba732577aafa72ce8087b
tonilingardsson <toni.lingardsson@gmail.com>
2025-11-07 15:31:38 +0100

    Implement Main method to calculate shapes' areas.

--- OOP3-Polymorfism/Program.cs
+++ OOP3-Polymorfism/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Geometry is the base class, and it points to its children: circle, square, and rectangle
+            Geometry circle = new Circle(); // Same type (Geometry)
+            Geometry square = new Square(); // but different objects
+            Geometry rectangle = new Rectangle();
+
+            // Store all the shapes in an array
+            Geometry[] shapes = { circle, square, rectangle};
+
+            // Polymophism in action: Same method call (Area()), different behavior.
+            // Each shape calculates area differentlly base on its actual type
+            // Loop through and call Area() on each shape
+            foreach (var shape in shapes)
+            {
+                // shape.Area() callst the correct override for each shape type
+                Console.WriteLine($"Area of {shape.GetType().Name}: {shape.Area(): F2}");
+            }
+        }
+    }
+}
