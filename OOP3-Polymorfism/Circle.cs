dfafee153b348a5a44671e4c7dcb8e3b7ee83df0
tonilingardsson <toni.lingardsson@gmail.com>
2025-11-07 15:32:27 +0100

    Add using directives to Circle and Geometry classes for improved functionality

--- OOP3-Polymorfism/Circle.cs
+++ OOP3-Polymorfism/Circle.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Polymorphism
 {
     public class Circle : Geometry
@@ -5,11 +11,13 @@ namespace Polymorphism
         // Declaring Radius property and its type
         public double Radius {get; set;}
 
+        // Constructor with Radius = 4 (fixed value)
         public Circle()
         {
             Radius = 4; // Fixed value 
         }
 
+    // Declaring Circle's method for Area
         public override double Area()
         {
             // Circle's area is π x r^2
--- OOP3-Polymorfism/Geometry.cs
+++ OOP3-Polymorfism/Geometry.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Polymorphism
 {
     // Abstract classes requires that children classes apply/override its methods
