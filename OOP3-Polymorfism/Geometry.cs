88367aaea5535a7318ce755c0924bc4ea45d13d4
tonilingardsson <toni.lingardsson@gmail.com>
2025-11-07 11:19:39 +0100

    Create a base class, Geometry with Area method. Abstract, children must follow

---
+++ OOP3-Polymorfism/Geometry.cs
@@ -0,0 +1,10 @@
+namespace Polymorphism
+{
+    // Abstract classes requires that children classes apply/override its methods
+    public abstract class Geometry
+    {
+        // Create an abstract method called Area()
+        public abstract double Area()
+        {}
+    }
+}
